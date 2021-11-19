782bd87f0b4c426b514b38f31bcf13e08fd8bbea
Edd Kleszcz <edd@Edds-MacBook-Pro.local>
2021-11-19 16:36:04 +0000

    Building up solution for test1

--- src/components/test1/Test1.jsx
+++ src/components/test1/Test1.jsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
 import "./Test1.css";
+const movieData = require("./movies.json");
 
 const Test1 = () => {
+    const [query, setQuery] = useState("");
+
     return (
         <section>
-
+            <h2>Movie List Filter:</h2>
+            <form for="movieQuery" />
+            <input id="movieQuery" value={query} onChange={(event) => setQuery(event.target.value)} />
+            <ul>
+                {movieData.map(movie => {
+                    if(movie.title.toLowerCase().includes(query.toLowerCase())) {
+                        return (
+                            <li key={movie.title}>
+                            {`${movie.title}, ${movie.genre}, ${movie.year}, ${movie.director}`}
+                        </li>
+                    )
+                }
+                })}
+            </ul>
         </section>
     )
 }
