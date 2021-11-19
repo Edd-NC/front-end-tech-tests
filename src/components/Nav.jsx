7a4bb195a55f25c2e40334006dbd959ecc182cf5
Edd Kleszcz <edd@Edds-MacBook-Pro.local>
2021-11-19 10:01:51 +0000

    Updating Navigation logic

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,17 +1,21 @@
-import {Link} from "react-router-dom"
+import "./Nav.css";
+import { Link } from "react-router-dom"
 
 const Nav = () => {
     const numOfTests = 6;
     const createLinks = () => {
         const links = [];
         for (let i = 1; i <= numOfTests; i++) {
-            links.push(<Link to={"Test " + i}><ul>{"Test " + i}</ul></Link>);
+            links.push(<li className="testLink"><Link to={"Test " + i}>{"Test " + i}</Link></li>);
         }
         return links;
     }
     return (
         <section>
+            <ul className="testLinks">
+                <li className="testLink"><Link to="">Home</Link></li>
                 {createLinks()}
+            </ul>
         </section>
     )
 }
