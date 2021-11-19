ffa3c20e9b75ddbad8c70248f1c4b14d455983c2
Edd Kleszcz <edd@Edds-MacBook-Pro.local>
2021-11-19 08:51:38 +0000

    Constructing new links with for loop

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,15 +3,15 @@ import {Link} from "react-router-dom"
 const Nav = () => {
     const numOfTests = 6;
     const createLinks = () => {
+        const links = [];
         for (let i = 1; i <= numOfTests; i++) {
-            <Link><ul>`Test {i}</ul></Link>
+            links.push(<Link to={"Test " + i}><ul>{"Test " + i}</ul></Link>);
         }
+        return links;
     }
     return (
         <section>
-            <ul>
                 {createLinks()}
-            </ul>
         </section>
     )
 }
