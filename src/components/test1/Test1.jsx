763b37872df669e85fe4f0eb4d8d645fc980458f
Edd Kleszcz <edd@edds-mbp.home>
2021-11-20 21:17:16 +0000

    Adding director querying

--- src/components/test1/Test1.jsx
+++ src/components/test1/Test1.jsx
@@ -3,18 +3,23 @@ import "./Test1.css";
 const movieData = require("./movies.json");
 
 const Test1 = () => {
-    const [query, setQuery] = useState("");
+    const [titleQuery, setTitleQuery] = useState("");
+    const [directorQuery, setDirectorQuery] = useState("");
 
     return (
         <section>
             <h2>Movie List Filter:</h2>
-            <form for="movieQuery" />
-            <input id="movieQuery" value={query} onChange={(event) => setQuery(event.target.value)} />
+            <form>
+            <label for="titleQuery">Query Title: </label><br />
+            <input id="titleQuery" value={titleQuery} onChange={(event) => setTitleQuery(event.target.value)} /><br />
+            <label for="directorQuery">Query Director: </label><br />
+            <input id="directorQuery" value={directorQuery} onChange={(event) => setDirectorQuery(event.target.value)} /><br />
+            </form>
             <br /><br />
                 <table>
                 <tr><th>Title</th><th>Director</th><th>Year</th><th>Genre</th><th></th></tr>
                 {movieData.map(movie => {
-                    if(movie.title.toLowerCase().includes(query.toLowerCase())) {
+                    if(movie.title.toLowerCase().includes(titleQuery.toLowerCase()) && movie.director.toLowerCase().includes(directorQuery.toLowerCase())) {
                         return (
                             <tr><td>{movie.title}</td><td>{movie.director}</td><td>{movie.year}</td><td>{movie.genre}</td></tr>
                         )
