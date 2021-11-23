c760a729c5e5027dd6d3bf4a5843e1de5053a214
Edd Kleszcz <edd@Edds-MacBook-Pro.local>
2021-11-23 10:34:14 +0000

    Building up drop down menu list

--- src/components/test5/Test5.jsx
+++ src/components/test5/Test5.jsx
@@ -1,7 +1,30 @@
+import "./Test5.css";
+
 const Test5 = () => {
     return (
         <section>
             <h2>Test 5</h2>
+            <section className="menu">
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Tests<ul>
+                    <li>Test 1</li>
+                    <li>Test 2</li>
+                    <li>Test 3</li>
+                    <li>Test 4</li>
+                    <li>Test 5</li>
+                    <li>Test 6</li>
+                    </ul></li>
+                <li>Companies<ul>
+                    <li>Northcoders</li>
+                    <li>Apple</li>
+                    <li>Google</li>
+                    <li>Twitter</li>
+                    </ul></li>
+                <li>Contact Us</li>
+            </ul>
+            </section>
         </section>
     )
 }
