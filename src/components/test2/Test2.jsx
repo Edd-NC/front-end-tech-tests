cca34dc165039ffd7d6d358d5c29b591efefa80f
Edd Kleszcz <edd@Edds-MacBook-Pro.local>
2021-12-07 12:44:36 +0000

    adding something

--- src/components/test2/Test2.jsx
+++ src/components/test2/Test2.jsx
@@ -8,12 +8,12 @@ const Test2 = () => {
     const [signUpDetails, setSignUpDetails] = useState({name: "", email: "", password: ""});
     return(
         <section>
-            <h2>Test 2</h2>
+            <h2>Create a Log In Page:</h2>
             <form id="loginForm">
                 <label for="email" >Email:</label><br />
-                <input id="email" type="text" value={email} onChange={(event) => setEmail(event.target.value)} required/><br /> <br />
+                <input id="email" type="text" value={email} onChange={(event) => setEmail(event.target.value)} autocomplete="off" required/><br /> <br />
                 <label for="password">Password:</label><br />
-                <input id="password" type="password" value={password} onChange={(event) => setPassword(event.target.value)} required/><br /><br />
+                <input id="password" type="password" value={password} onChange={(event) => setPassword(event.target.value)} autocomplete="off" required/><br /><br />
                 <button type="submit" for="loginForm">Submit</button><br /><br />
             </form>
             <button onClick={() => setShowSignUp(signUp => !signUp)}>Sign Up?</button><br /><br />
@@ -23,20 +23,20 @@ const Test2 = () => {
                         <input id="nameSignUp" type="text" value={signUpDetails.name} onChange={(event) => setSignUpDetails(details => {
                             const name = event.target.value;
                             const newDetails = {...details, name};
-                            return newDetails})} required/><br /><br />
+                            return newDetails})} autocomplete="off" required/><br /><br />
                         <label for="emailSignUp">Your Email:</label><br />
                         <input id="emailSignUp" type="text" value={signUpDetails.email} onChange={(event) => setSignUpDetails(details => {
                             const email = event.target.value;
                             const newDetails = {...details, email};
                             return newDetails;
-                        })} required/><br /><br />
+                        })} autocomplete="off" required/><br /><br />
                         <label for="passwordSignUp">Password:</label><br />
                         <input id="passwordSignUp" type="password" value={signUpDetails.password} onChange={event => setSignUpDetails(details => {
                             const password = event.target.value;
                             const newDetails = {...details, password};
                             return newDetails;
-                        })} required/><br /><br />
-                        <button type="submit" for="signUpForm">Sign Up</button> 
+                        })} autocomplete="off" required/><br /><br />
+                        <button type="submit" for="signUpForm">Submit</button> 
                     </form>
                 </section>}
         </section>
