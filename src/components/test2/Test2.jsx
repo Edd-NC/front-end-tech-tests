ccd8b6891090cafa3aebdd0c27235a307111cfed
Edd Kleszcz <edd@Edds-MacBook-Pro.local>
2021-12-07 10:11:38 +0000

    Building up test 2

--- src/components/test2/Test2.jsx
+++ src/components/test2/Test2.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import "./Test2.css";
 
 const Test2 = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
     return(
         <section>
             <h2>Test 2</h2>
+            <form>
+                <label for="email" >Email:</label><br />
+                <input id="email" type="text" value={email} onChange={(event) => setEmail(event.target.value)}/><br /> <br />
+                <label for="password">Password:</label><br />
+                <input id="password" type="password" value={password} onChange={(event) => setPassword(event.target.value)}/>
+            </form>
         </section>
     )
 }
