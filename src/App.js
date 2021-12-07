6bea1af81b83dc50be179a8f94e1e8af4b7db9e2
Edd Kleszcz <edd@Edds-MacBook-Pro.local>
2021-12-07 09:15:32 +0000

    Adding routing for 4 other tech tests

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import Header from './components/Header';
 import Nav from './components/Nav';
 import Home from './components/Home.jsx';
 import Test1 from './components/test1/Test1.jsx';
+import Test2 from './components/test2/Test2.jsx';
+import Test3 from './components/test3/Test3.jsx';
+import Test4 from './components/test4/Test4.jsx';
 import Test5 from './components/test5/Test5.jsx';
+import Test6 from './components/test6/Test6.jsx';
 
 function App() {
   return (
@@ -14,7 +18,11 @@ function App() {
        <Routes>
           <Route exact path="/" element={Home()} />
           <Route exact path="/test/1" element={Test1()} />
+          <Route exact path="/test/2" element={Test2()} />
+          <Route exact path="/test/3" element={Test3()} />
+          <Route exact path="/test/4" element={Test4()} />
           <Route exact path="/test/5" element={Test5()} />
+          <Route exact path="/test/6" element={Test6()} />
        </Routes>
     </section>
   );
