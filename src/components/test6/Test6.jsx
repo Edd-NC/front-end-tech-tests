5f5fa93f3e5edbdd38f23352aec182d5e5c246cd
Edd Kleszcz <edd@Edds-MacBook-Pro.local>
2021-12-08 14:43:19 +0000

    adding instructions

--- src/components/test6/Test6.jsx
+++ src/components/test6/Test6.jsx
@@ -1,9 +1,27 @@
 import "./Test6.css";
+import "../instructionsButton.css";
+import { useState } from "react";
 
 const Test6 = () => {
+    const [showInstructions, setShowInstructions] = useState(false);
     return (
         <section>
             <h2>Build a Pseudo Instant Messaging Service</h2>
+            <button className="instructionsButton" onClick={() => setShowInstructions(bool => !bool)}>Show/Hide Instructions</button><br /><br />
+            {showInstructions && <section>
+                <hr />
+                    <h3>Instructions:</h3>
+                    <p>Your task is to build messaging service, on one screen!</p>
+                    <h4>You must complete the following:</h4>
+                    <ul>
+                        <li>Build a box that contains all the messages and who sent them in chronological order,</li>
+                        <li>Build 2 seperate chat boxes where a user can type a message and send it to the general chat box,</li>
+                    </ul>
+                    <h4>Challenge:</h4>
+                    <p></p>
+                <hr />
+                </section>}
+
         </section>
     )
 }
