88dec1613853b7456379bf7362af0e8d81f6d49f
Edd Kleszcz <edd@Edds-MacBook-Pro.local>
2021-12-08 14:09:42 +0000

    Adding instructions to tech test 3

--- src/components/test3/Test3.jsx
+++ src/components/test3/Test3.jsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 
 const Test3 = () => {
     const states = ['Alabama','Alaska', 'Arizona','Arkansas','California','Colorado','Connecticut','Delaware','Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming'];
+
+    const [showInstructions, setShowInstructions] = useState(false);
     const [answer, setAnswer] = useState("");
     const [correctAnswers, setCorrectAnswers] = useState([]);
 
@@ -24,6 +26,24 @@ const Test3 = () => {
     return (
         <section>
             <h2>50 US States Game:</h2>
+            <button className="instructionsButton" onClick={() => setShowInstructions(bool => !bool)}>Show/Hide Instructions</button><br /><br />
+            {showInstructions && <section>
+                <hr />
+                    <h3>Instructions:</h3>
+                    <p>Your task is to build a mini game where a player can guess all 50 US states...</p>
+                    <h4>You must complete the following:</h4>
+                    <ul>
+                        <li>Create an input box where the user can type any case insensitive state and it will accept the answer,</li>
+                        <li>Create text box that will store all the correct answers the player has given,</li>
+                        <li>The text box should not accept duplicate values, regardless of case sensitvity,</li>
+                        <li>Create a restart button that will reset the game,</li>
+                        <li>A give up button that will show the remaining answers that the player did not guess,</li>
+                        <li>A counter that will tell the player how many states he has correctly guessed</li>
+                    </ul>
+                    <h4>Challenge:</h4>
+                    <p>Add a timer that when it reaches 0 will disable the input box until restart is pressed</p>
+                <hr />
+                </section>}
             <button className="t3button" onClick={() => {
                 setCorrectAnswers([]);
                 setAnswer("");
@@ -34,7 +54,7 @@ const Test3 = () => {
                 
             }} /> <br /><br />
             <p>Total: {correctAnswers.length} / 50</p>
-            <textarea cols="100" rows="10" value={correctAnswers.join(", ")} /><br /><br />
+            <textarea cols="100" rows="10" value={correctAnswers.join(", ")} disabled /><br /><br />
             <button className="t3button" onClick={() => {
                 states.forEach(state => {
                     if (!correctAnswers.includes(state)) {
