8985a20b2a0bdb66bc97522b6aa5a32b374b3cb6
Edd Kleszcz <edd@Edds-MacBook-Pro.local>
2021-12-08 15:24:52 +0000

    Formatting user 1 and adding user 2 chat

--- src/components/test6/Test6.jsx
+++ src/components/test6/Test6.jsx
@@ -5,6 +5,8 @@ import { useState } from "react";
 const Test6 = () => {
     const [showInstructions, setShowInstructions] = useState(false);
     const [genChat, setGenChat] = useState([]);
+    const [chat1, setChat1] = useState("");
+    const [chat2, setChat2] = useState("");
     return (
         <section>
             <h2>Pseudo Instant Messaging Service</h2>
@@ -23,8 +25,34 @@ const Test6 = () => {
                 <hr />
                 </section>}
                 <section>
+                    <form id="chat1Form" onSubmit={event => {
+                         event.preventDefault(); 
+                         setGenChat(genChat => {
+                             const newGenChat = [...genChat];
+                             newGenChat.push("User 1: " + chat1)
+                             return newGenChat;
+                         })
+                         setChat1("");
+                    }}>
+                    <label for="chat1">User 1:</label><br />
+                    <textarea id="chat1" value={chat1} onChange={(event) => setChat1(event.target.value)} cols="20" rows="20"/>
+                    <button className="chatSendButton" for="chat1Form">Send</button>
+                    </form>
                     <label for="genChat">General Chat:</label><br />
-                    <textarea id="genChat" value={genChat.join("\n")} cols="70" rows="20"></textarea>
+                    <textarea id="genChat" value={genChat.join("\n")} cols="70" rows="20" />
+                    <form id="chat2Form" onSubmit={event => {
+                         event.preventDefault(); 
+                         setGenChat(genChat => {
+                             const newGenChat = [...genChat];
+                             newGenChat.push("User 2: " + chat2)
+                             return newGenChat;
+                         })
+                         setChat2("");
+                    }}>
+                    <label for="chat2">User 2:</label><br />
+                    <textarea id="chat2" value={chat2} onChange={(event) => setChat2(event.target.value)} cols="20" rows="20"/>
+                    <button className="chatSendButton" for="chat2Form">Send</button>
+                    </form>
                 </section>
         </section>
     )
