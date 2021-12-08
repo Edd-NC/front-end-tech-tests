df1fad6c89ef2e34bcec8016215e425d97f23633
Edd Kleszcz <edd@Edds-MacBook-Pro.local>
2021-12-08 13:54:31 +0000

    adding instructions to test 2

--- src/components/test2/Test2.jsx
+++ src/components/test2/Test2.jsx
@@ -2,6 +2,7 @@ import { useContext, useState } from "react";
 import "./Test2.css";
 
 const Test2 = () => {
+    const [showInstructions, setShowInstructions] = useState(false);
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [showSignUp, setShowSignUp] = useState(false);
@@ -9,6 +10,21 @@ const Test2 = () => {
     return(
         <section>
             <h2>Create a Log In Page:</h2>
+            <button className="instructionsButton" onClick={() => setShowInstructions(bool => !bool)}>Show/Hide Instructions</button><br /><br />
+            {showInstructions && <section>
+                <hr />
+                    <h3>Instructions:</h3>
+                    <p>Your task is to build a login form where a user input their email and password to enter a website. Or, where they can sign up.</p>
+                    <h4>You must complete the following:</h4>
+                    <ul>
+                        <li>Create a log in form where a user can put in their details,</li>
+                        <li>Create a sign up form where a new user can create an account, using their name, email and password,</li>
+                        <li>Hide the sign up info unless they click a button where they can sign up</li>
+                    </ul>
+                    <h4>Challenge</h4>
+                    <p>Add in input validation to ensure that their email is of the correct format and their password contains a number, a lowercase letter and an uppercase letter</p>
+                <hr />
+                </section>}
             <form id="loginForm">
                 <label for="email" >Email:</label><br />
                 <input id="email" type="text" value={email} onChange={(event) => setEmail(event.target.value)} autocomplete="off" required/><br /> <br />
