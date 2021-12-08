9ef7b426db837128a3acc70214aff96d373b0182
Edd Kleszcz <edd@Edds-MacBook-Pro.local>
2021-12-08 14:11:10 +0000

    adding instructions

--- src/components/test4/Test4.jsx
+++ src/components/test4/Test4.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import "./Test4.css";
 
 const Test4 = () => {
+    const [showInstructions, setShowInstructions] = useState(false);
     const [newGame, setNewGame] = useState([]);
     const [found, setFound] = useState(0);
     const [score, setScore] = useState("");
@@ -27,6 +28,25 @@ const Test4 = () => {
     return (
         <section>
             <h2>Find the Hidden X Game:</h2>
+            <button className="instructionsButton" onClick={() => setShowInstructions(bool => !bool)}>Show/Hide Instructions</button><br /><br />
+            {showInstructions && <section>
+                <hr />
+                    <h3>Instructions:</h3>
+                    <p>Your task is to build a mini game where a player can guess all 50 US states...</p>
+                    <h4>You must complete the following:</h4>
+                    <ul>
+                        <li>Create an input box where the user can type any case insensitive state and it will accept the answer,</li>
+                        <li>Create text box that will store all the correct answers the player has given,</li>
+                        <li>The text box should not accept duplicate values, regardless of case sensitvity,</li>
+                        <li>Create a restart button that will reset the game,</li>
+                        <li>A give up button that will show the remaining answers that the player did not guess,</li>
+                        <li>A counter that will tell the player how many states he has correctly guessed</li>
+                    </ul>
+                    <h4>Challenge:</h4>
+                    <p>Add a timer that when it reaches 0 will disable the input box until restart is pressed</p>
+                <hr />
+                </section>}
+
             <p>{score}</p>
             <table className="gameTable">
             {newGame.map(row => {
